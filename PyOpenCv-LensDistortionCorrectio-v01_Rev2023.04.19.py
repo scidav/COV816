201c09751303e780bd751f575841ae37939b1e59
Irving D. Hernández <i.davidhf@outlook.com>
2023-04-19 22:27:35 -0300

    Create PyOpenCv-LensDistortionCorrectio-v01_Rev2023.04.19.py

---
+++ PyOpenCv-LensDistortionCorrectio-v01_Rev2023.04.19.py
@@ -0,0 +1,62 @@
+import numpy as np
+import cv2 as cv
+from datetime import datetime
+import time
+import os
+
+
+# Calibration file directory
+directorio_de_trabajo = "C:/Temp/CalibrationImages"
+os.chdir(o_pth)
+
+# Method 02 - Loading stored parameters
+def loadMatrices(fileName):
+    with open(fileName, 'rb') as f:
+        A = np.load(f)
+        B = np.load(f)
+        C = np.load(f)
+        D = np.load(f)
+    return (A,B,C,D)
+
+mtx, dist, rv, tv = loadMatrices("calibrationParameters.npy")
+
+
+
+# Camera configuration
+cap = cv.VideoCapture(0, cv.CAP_DSHOW) # this is the magic!
+hres,vres = 1280, 720
+cap.set(cv.CAP_PROP_FRAME_WIDTH, hres)
+cap.set(cv.CAP_PROP_FRAME_HEIGHT,vres)
+
+
+if not cap.isOpened():
+    print("Não é possível mostrar a camera")
+    exit()
+
+contador = 0
+
+# Image acquisition
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    # if frame is read correctly ret is True
+    if not ret:
+        print("No es posible retener algun Frame (Finalizar transmisión?). Saliendo ...")
+        break
+    #
+    # Mostrar a imagen capturada
+    sf = 0.5
+    dImage  = frame
+    uImage  = cv.undistort(dImage, mtx, dist, None, None)
+    dIma_r  = cv.resize(dImage, (int(sf*hres),int(sf*vres)))
+    uIma_r  = cv.resize(uImage, (int(sf*hres),int(sf*vres)))
+    cv.imshow('Imagen Capturada', np.hstack([dIma_r,uIma_r]))
+    # =====================
+    time.sleep(0.1)
+    # =====================
+    if cv.waitKey(1) == ord('q'):
+        break
+
+# Cuando todo esté listo, liberar camara y cerrar todas las ventanas
+cap.release()
+cv.destroyAllWindows()
